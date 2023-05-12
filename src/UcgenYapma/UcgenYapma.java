8c62d10e0a7afd951fe297cf59759eafe108f305
Mustafa Akaydın <akaydinmustafa5@gmail.com>
2023-05-12 19:16:41 +0300

    Üçgen yapma

---
+++ src/UcgenYapma/UcgenYapma.java
@@ -0,0 +1,22 @@
+package UcgenYapma;
+
+import java.util.Scanner;
+
+public class UcgenYapma {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Bir sayı girin: ");
+        int n = input.nextInt();
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
