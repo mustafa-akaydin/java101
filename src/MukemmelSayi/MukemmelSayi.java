d16994ded10682e6b69ef3b7e42a6bd34cf30aa3
Mustafa Akaydın <akaydinmustafa5@gmail.com>
2023-05-12 19:13:40 +0300

    Mükemmel Sayı

---
+++ src/MukemmelSayi/MukemmelSayi.java
@@ -0,0 +1,20 @@
+package MukemmelSayi;
+import java.util.Scanner;
+public class MukemmelSayi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int number = input.nextInt();
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        if (sum == number) {
+            System.out.println(number + " mükemmel sayıdır.");
+        } else {
+            System.out.println(number + " mükemmel sayı değildir.");
+        }
+    }
+}
